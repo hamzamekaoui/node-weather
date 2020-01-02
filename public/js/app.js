8636844ce132d924e79a87d0b2b09b31134da04c
Hamza Mekaoui <mekaoui.dev@gmail.com>
2020-01-02 23:17:53 +0100

    added documentation

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+
+// get By id
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector("#message-2")
 
